9c2ea355ca3f8a7b5a1ca7674e693f96b0d2836a
Eduardoooo0 <el957733@gmail.com>
2024-09-21 12:19:02 -0300

    adicionando o login e cadastro

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, make_response
+from flask_login import LoginManager, login_required, login_user, logout_user
+from models import User, obter_conexao
+from werkzeug.security import generate_password_hash, check_password_hash
+
+login_manager = LoginManager()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'VASCODAGAMA'
+
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
 
 @app.route('/')
 def index():
@@ -20,3 +33,32 @@ def ficha():
 @app.route('/agendadas')
 def agendadas():
     return render_template('agendadas.html')
+
+@app.route('/cadastro', methods = ['GET','POST'])
+def cadastro():
+    if request.method == 'GET':
+        return render_template('cadastro.html')
+    else:
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        senha_hash = generate_password_hash(senha)
+        if not User.exists(email):
+            user = User(email=email,senha=senha_hash)
+            user.save()
+            login_user(user)
+            return redirect(url_for('index'))
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        user = User.get_by_email(email)
+        if user and check_password_hash(user['senha'],senha):
+            login_user(User.get(user['id']))
+            return redirect(url_for('index'))
+        else:
+            flash('Dados incorretos')
+            return render_template('login.html')
